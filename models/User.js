0cca923f5d2f2c037bf5fe20ef5d1852f3817e01
Pablo Nunes <cpepablito@gmail.com>
2020-07-29 20:16:33 -0300

    Removing idea

--- models/User.js
+++ models/User.js
@@ -1,3 +1,16 @@
+const _ = require('lodash');
+
+function InvalidUserException(message) {
+    this.message = message;
+    if ("captureStackTrace" in Error)
+        Error.captureStackTrace(this, InvalidUserException);
+    else
+        this.stack = (new Error()).stack;
+}
+InvalidUserException.prototype = Object.create(Error.prototype);
+InvalidUserException.prototype.name = "InvalidUserException";
+InvalidUserException.prototype.constructor = InvalidUserException;
+
 /**
  * @swagger
  *  components:
@@ -22,3 +35,22 @@
  *           email: fake@email.com
  *           password: fake_password
  */
+
+class User{
+    constructor(object) {
+            this.validate(object)
+            this.id = object.id || ''
+            this.name = object.name
+            this.email = object.email
+            this.password = object.password
+    }
+
+    validate(user){
+        if (_.isEmpty(user.name))
+            throw new InvalidUserException('Empty User Name')
+        if (_.isEmpty(user.email))
+            throw new InvalidUserException('Empty User Email')
+        if (_.isEmpty(user.password))
+            throw new InvalidUserException('Empty User Password')
+    }
+}
